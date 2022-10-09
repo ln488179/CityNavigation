90d077e4c59f3f37af319451017d9005d09c3078
Luyen Nguyen <ln488179@southeast.edu>
2022-10-09 00:31:00 -0500

    done

--- App.js
+++ App.js
@@ -2,17 +2,36 @@ import * as React from 'react';
 import { Image, View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import Button from './Button';
 import 'react-native-gesture-handler';
+import * as WebBrowser from 'expo-web-browser';
+
 import art from './assets/art.png';
 import mile from './assets/mile.png';
 import pier from './assets/pier.png';
 import water from './assets/water.png';
 import willis from './assets/willis.png';
 
+const artUrl = 'https://www.artic.edu/';
+const mileUrl = 'https://www.themagnificentmile.com/';
+const pierUrl = 'https://navypier.org/';
+const waterUrl = 'https://www.chicago.gov/city/en/depts/dca/supp_info/city_gallery_in_thehistoricwatertower.html';
+const willisUrl = 'https://www.willistower.com/';
+
+function handleButtonPress (url) {
+  WebBrowser.openBrowserAsync(url);
+}
+
 function ArtInstituteOfChicago() {
   return (
     <View style={styles.container}>
       <Image source={art} style={styles.image} />
+
+      <Button info style={styles.button}
+        onPress={()=> handleButtonPress(artUrl)}>
+        More Information
+      </Button>
+      
     </View>
   );
 }
@@ -21,6 +40,11 @@ function MagnificentMile() {
   return (
     <View style={styles.container}>
       <Image source={mile} style={styles.image} />
+
+      <Button info style={styles.button}
+        onPress={()=> handleButtonPress(mileUrl)}>
+        More Information
+      </Button>
     </View>
   );
 }
@@ -29,6 +53,11 @@ function NavyPier() {
   return (
     <View style={styles.container}>
       <Image source={pier} style={styles.image} />
+
+      <Button info style={styles.button}
+        onPress={()=> handleButtonPress(pierUrl)}>
+        More Information
+      </Button>
     </View>
   );
 }
@@ -37,6 +66,11 @@ function WaterTower() {
   return (
     <View style={styles.container}>
       <Image source={water} style={styles.image} />
+      
+      <Button info style={styles.button}
+        onPress={()=> handleButtonPress(waterUrl)}>
+        More Information
+      </Button>
     </View>
   );
 }
@@ -45,6 +79,11 @@ function WillisTower() {
   return (
     <View style={styles.container}>
       <Image source={willis} style={styles.image} />
+      
+      <Button info style={styles.button}
+        onPress={()=> handleButtonPress(willisUrl)}>
+        More Information
+      </Button>
     </View>
   );
 }
@@ -71,6 +110,7 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -79,8 +119,11 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   image: {
-    width: 320  , 
-    height: 480
+    width: 240  , 
+    height: 360
+  },
+  button:{
+    margin: 20,
   },
 
 });
